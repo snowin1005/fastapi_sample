fceebb53d48f0a6775ba37ce216f47805c31b2f0
snowin <snowin1005@gmail.com>
2021-01-24 14:11:38 +0900

    add Link

---
+++ react_router/src/js/pages/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="/archives">archives</Link>,
+        <Link to="/settings">settings</Link>
+      </div>
+    );
+  }
+}
+
