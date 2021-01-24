1fe59d2690d9f406e3513a9be6d63199e6133601
snowin <snowin1005@gmail.com>
2021-01-24 19:15:33 +0900

    add link query

--- react_router/src/js/pages/Archives.js
+++ react_router/src/js/pages/Archives.js
@@ -2,8 +2,13 @@ import React from "react"
 
 export default class Archives extends React.Component {
     render() {
+        const query = new URLSearchParams(this.props.location.search)
+        let message 
+            = (this.props.match.article ? this.props.match.params.article + ", " : "")
+            + "date=" + query.get("date") + ", filter=" + query.get("filter");
+        console.log(this.props);
         return (
-            <h1>Archives</h1>
+            <h1>Archives ({message})</h1>
         );
     }
 }
