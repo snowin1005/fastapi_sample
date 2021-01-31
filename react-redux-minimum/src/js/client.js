a07cb3f17be4e41efc0028c29a34d6fb6264dbf2
snowin <snowin1005@gmail.com>
2021-01-31 23:36:22 +0900

    add reducer

--- react-redux-minimum/src/js/client.js
+++ react-redux-minimum/src/js/client.js
@@ -1,1 +1,24 @@
-// import { createStore } from "redux";
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case "INC":
+            return state + action.payload;
+        case "DEC":
+            return state - action.payload;
+    }
+    return state;
+    // console.log("reducer has been called.");
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log("store changed", store.getState());
+});
+
+store.dispatch({ type: "INC", payload: 1 });
+store.dispatch({ type: "INC", payload: 2 });
+store.dispatch({ type: "INC", payload: 22 });
+store.dispatch({ type: "INC", payload: 222 });
+store.dispatch({ type: "DEC", payload: 1000 });
