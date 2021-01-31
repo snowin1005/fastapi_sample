cf4eb01ad0646d113594b722cbb6a42999c41987
snowin <snowin1005@gmail.com>
2021-01-31 21:55:38 +0900

    fix

--- todolist/src/js/pages/Todos.js
+++ todolist/src/js/pages/Todos.js
@@ -1,35 +1,60 @@
 import React from "react";
 
 import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoAction";
+import TodoStore from "../stores/TodoStore";
 
 export default class Todos extends React.Component {
   constructor() {
     super();
+    this.getTodos = this.getTodos.bind(this);
     this.state = {
-      todos: [
-        {
-          id: 113464613,
-          text: "Go Shopping",
-          complete: false
-        },
-        {
-          id: 235684679,
-          text: "Pay Bills",
-          complete: false
-        }
-      ]
-    };
+      todos: TodoStore.getAll()
+    }
+  }
+
+  componentDidMount() {
+    // todoStore.on("change", () => {
+    //   this.setState({
+    //     todos: TodoStore.getAll()
+    //   });
+    //   console.log("componentDidMount.change");
+    //   console.log(this.state);
+    // });
+    TodoStore.on("change", this.getTodos);
+    console.log("count", TodoStore.listenerCount("change"));
+  }
+
+  componentWillUnmount() {
+    TodoStore.removeListener("change", this.getTodos);
+  }
+
+  getTodos() {
+    this.setState({
+      todos: TodoStore.getAll()
+    });
+  }
+
+  createTodo() {
+    TodoActions.createTodo("New Todo");
+  }
+
+  reloadTodo() {
+    TodoActions.reloadTodo();
   }
 
   render() {
     const { todos } = this.state;
-
+    console.log("render");
+    console.log(this.state);
     const TodoComponents = todos.map((todo) => {
-      return <Todo key={todo.id} {...todo}/>;
+      return <Todo key={todo.id} {...todo} />;
     });
 
     return (
       <div>
+        <button onClick={this.createTodo.bind(this)}>Create!</button>
+        <button onClick={this.reloadTodo.bind(this)}>Reload!</button>
         <h1>Todos</h1>
         <ul>{TodoComponents}</ul>
       </div>
--- todolist/src/js/stores/TodoStore.js
+++ todolist/src/js/stores/TodoStore.js
@@ -1,1 +1,73 @@
 /* This file will be filled with on the lesson */
+import { EventEmitter } from "events";
+
+import dispatcher from "../dispatcher";
+
+class TodoStore extends EventEmitter {
+    constructor() {
+        super();
+        this.todos = [
+            {
+                id: 113464613,
+                text: "Go Shopping",
+                complete: false
+            },
+            {
+                id: 235684679,
+                text: "Pay Watter Bills",
+                complete: false
+            }
+        ];
+    }
+
+    createTodo(text) {
+        const id = Date.now();
+        this.todos.push({
+            id,
+            text,
+            complete: false
+        });
+        console.log("createTodo");
+        console.log(this.todos);
+        this.emit("change");
+    }
+
+    receiveTodos(todos) {
+        console.log("receiveTodos");
+        console.log(this.todos);
+        this.todos = todos;
+        console.log(this.todos);
+        this.emit("change");
+    }
+
+    getAll() {
+        console.log("getAll");
+        console.log(this.todos);
+        return this.todos;
+    }
+
+    handleActions(action) {
+        switch (action.type) {
+            case "CREATE_TODO": {
+                console.log("CREATE_TODO");
+                this.createTodo(action.text);
+                break;
+            }
+            case "RECEIVE_TODOS": {
+                console.log("RECEIVE_TODOS");
+                this.receiveTodos(action.todos);
+                break;
+            }
+        }
+        console.log("handleActions");
+        console.log(this.todos);
+        console.log("TodoStore received an action", action);
+    }
+
+}
+
+const todoStore = new TodoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+window.todoStore = todoStore;
+window.dispatcher = dispatcher;
+export default todoStore;
