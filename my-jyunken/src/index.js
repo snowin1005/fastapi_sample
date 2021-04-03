ec93415b5f56edf584254c7a18b68e486f5889d2
snowin <snowin1005@gmail.com>
2021-04-04 03:51:02 +0900

    add my-jyunken

---
+++ my-jyunken/src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const MoneyBook = () => {
+  const books = [
+    { date: "1/1", item: "お年玉", amount: 10000 },
+    { date: "1/3", item: "ケーキ", amount: -500 },
+    { date: "2/1", item: "小遣い", amount: 3000 },
+    { date: "2/5", item: "マンガ", amount: -600 }
+  ]
+  return (
+    <div>
+      <h1>小遣い帳</h1>
+      <table className="book">
+        <thead>
+          <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
+        </thead>
+        <tbody>
+          <MoneyBookItem book={books[0]} />
+          <MoneyBookItem book={books[1]} />
+          <MoneyBookItem book={books[2]} />
+          <MoneyBookItem book={books[3]} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const MoneyBookItem = (props) => {
+  const { date, item, amount } = props.books
+  if (amount > 0) {
+    return (
+      <tr>
+        <td>{date}</td>
+        <td>{item}</td>
+        <td>{amount}</td>
+        <td></td>
+      </tr>
+    )
+  } else {
+    return (
+      <tr>
+        <td>{date}</td>
+        <td>{item}</td>
+        <td></td>
+        <td>{-amount}</td>
+      </tr>
+    )
+  }
+}
+
+MoneyBookItem.prototype = {
+  book: PropTypes.object.isRequired
+}
+
+ReactDOM.render(
+  <MoneyBook />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
